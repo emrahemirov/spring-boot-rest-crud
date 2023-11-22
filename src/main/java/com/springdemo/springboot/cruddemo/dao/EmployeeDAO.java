a50120a0cce71fa8e3b80e6a8d8bae5301c495cb
emrahemirov <103997751+emrahemirov@users.noreply.github.com>
2023-11-22 16:12:43 +0300

    ready to push

---
+++ src/main/java/com/springdemo/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,26 @@
+package com.springdemo.springboot.cruddemo.dao;
+
+import com.springdemo.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
+
+
+
+
+
+
+
+
+
+
